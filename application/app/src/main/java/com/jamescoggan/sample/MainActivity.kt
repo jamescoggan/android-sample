23b0700070e9e1dd7facc1b7dbdfb6f9c713375b
James Coggan <james.coggan@gmail.com>
2024-10-15 11:23:54 +0200

    Add Navigation to the app flow

--- application/app/src/main/java/com/jamescoggan/sample/MainActivity.kt
+++ application/app/src/main/java/com/jamescoggan/sample/MainActivity.kt
@@ -8,10 +8,17 @@ import androidx.appcompat.app.AppCompatActivity
 import com.jamescoggan.sample.di.ActivityKey
 import com.jamescoggan.sample.di.AppScope
 import com.jamescoggan.sample.ui.theme.AndroidAppSampleTheme
+import com.slack.circuit.backstack.rememberSaveableBackStack
 import com.slack.circuit.foundation.Circuit
 import com.slack.circuit.foundation.CircuitCompositionLocals
-import com.slack.circuit.foundation.CircuitContent
+import com.slack.circuit.foundation.NavigableCircuitContent
+import com.slack.circuit.foundation.rememberCircuitNavigator
+import com.slack.circuit.overlay.ContentWithOverlays
+import com.slack.circuitx.android.AndroidScreen
+import com.slack.circuitx.android.IntentScreen
+import com.slack.circuitx.android.rememberAndroidScreenAwareNavigator
 import com.squareup.anvil.annotations.ContributesMultibinding
+import kotlinx.collections.immutable.persistentListOf
 import javax.inject.Inject
 
 @ContributesMultibinding(AppScope::class, boundType = Activity::class)
@@ -21,10 +28,28 @@ class MainActivity @Inject constructor(private val circuit: Circuit) : AppCompat
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        val initialBackstack = persistentListOf(HomeScreen())
+
         setContent {
             AndroidAppSampleTheme {
-                CircuitCompositionLocals(circuit) { CircuitContent(HomeScreen()) }
+                val backStack = rememberSaveableBackStack(initialBackstack)
+                val circuitNavigator = rememberCircuitNavigator(backStack)
+                val navigator = rememberAndroidScreenAwareNavigator(circuitNavigator, this::goTo)
+
+                CircuitCompositionLocals(circuit) {
+                    ContentWithOverlays {
+                        NavigableCircuitContent(
+                            navigator = navigator,
+                            backStack = backStack,
+                        )
+                    }
+                }
             }
         }
+    }
+
+    private fun goTo(screen: AndroidScreen) = when (screen) {
+        is IntentScreen -> screen.startWith(this)
+        else -> error("Unknown AndroidScreen: $screen")
     }
 }
