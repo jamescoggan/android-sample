fdcdbbe25ad20bf664fac18f78afd6b7cf81afd5
James Coggan <james.coggan@gmail.com>
2024-10-15 11:41:10 +0200

    Add close button to recipes details so we can go back

--- application/app/src/main/java/com/jamescoggan/sample/recipes/RecipeDetailsScreen.kt
+++ application/app/src/main/java/com/jamescoggan/sample/recipes/RecipeDetailsScreen.kt
@@ -1,5 +1,7 @@
 package com.jamescoggan.sample.recipes
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
@@ -7,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -16,6 +20,8 @@ import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -43,7 +49,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class RecipeDetailsScreen(val recipeId: Int) : Screen {
-    data class State(val recipe: RecipeUiItem) : CircuitUiState
+    data class State(val recipe: RecipeUiItem, val eventSink: (Event) -> Unit) : CircuitUiState
 
     sealed interface Event : CircuitUiEvent {
         data object GoBack : Event
@@ -51,7 +57,6 @@ data class RecipeDetailsScreen(val recipeId: Int) : Screen {
 }
 
 class RecipeDetailsPresenter @AssistedInject constructor(
-    @Assisted private val screen: RecipeDetailsScreen,
     @Assisted private val navigator: Navigator,
 ) : Presenter<RecipeDetailsScreen.State> {
     @Composable
@@ -59,13 +64,17 @@ class RecipeDetailsPresenter @AssistedInject constructor(
         // TODO: get from datasource using ID
         return RecipeDetailsScreen.State(
             recommendedRecipesSamples[0]
-        )
+        ) { event ->
+            when (event) {
+                RecipeDetailsScreen.Event.GoBack -> navigator.pop()
+            }
+        }
     }
 
     @CircuitInject(RecipeDetailsScreen::class, AppScope::class)
     @AssistedFactory
     interface Factory {
-        fun create(screen: RecipeDetailsScreen, navigator: Navigator): RecipeDetailsPresenter
+        fun create(navigator: Navigator): RecipeDetailsPresenter
     }
 }
 
@@ -77,19 +86,18 @@ fun RecipeDetailsContent(state: RecipeDetailsScreen.State, modifier: Modifier = 
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         contentWindowInsets = WindowInsets(0, 0, 0, 0),
         topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text(
-                        state.recipe.title,
-                        fontSize = 22.sp,
-                        color = MaterialTheme.colorScheme.onBackground
-                    )
-                },
+            CenterAlignedTopAppBar(title = {
+                Text(
+                    state.recipe.title,
+                    fontSize = 22.sp,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            },
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.background
                 ),
                 scrollBehavior = scrollBehavior,
-            )
+                navigationIcon = { CloseButton({ state.eventSink(RecipeDetailsScreen.Event.GoBack) }) })
         },
     ) { paddingValues ->
         Box(
@@ -114,14 +122,22 @@ fun RecipeDetailsContent(state: RecipeDetailsScreen.State, modifier: Modifier = 
     }
 }
 
+@Composable
+fun CloseButton(onclick: () -> Unit, modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.clickable { onclick() },
+        painter = rememberVectorPainter(image = Icons.Filled.Close),
+        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+        contentDescription = "Close",
+    )
+}
+
 @Preview
 @Composable
 fun RecipeDetailsScreenPreview() {
     AndroidAppSampleTheme {
-        RecipeDetailsContent(
-            RecipeDetailsScreen.State(
-                recommendedRecipesSamples[0]
-            )
-        )
+        RecipeDetailsContent(RecipeDetailsScreen.State(
+            recommendedRecipesSamples[0]
+        ) {})
     }
 }
