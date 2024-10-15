8b9ae99fb74320f70bb560841171ee729694ddc4
James Coggan <james.coggan@gmail.com>
2024-10-15 15:32:43 +0200

    Remap the Recipe from the data model to the RecipeUiItem

--- application/app/src/main/java/com/jamescoggan/sample/ui/models/RecipeUiItem.kt
+++ application/app/src/main/java/com/jamescoggan/sample/ui/models/RecipeUiItem.kt
@@ -2,15 +2,22 @@ package com.jamescoggan.sample.ui.models
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import com.jamescoggan.data.model.Recipe
 import com.jamescoggan.sample.R
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 
-enum class RecipeCategory {
-    CHINESE, SPANISH, INDIAN
+enum class RecipeCategory(@Suppress("unused") val categoryName: String) {
+    CHINESE("chinese"), SPANISH("spanish"), INDIAN("indian");
 }
 
 
 data class RecipeUiItem(
-    val id: Int, val title: String, val imageUrl: String, val category: RecipeCategory, val cookingSteps: String
+    val id: Int,
+    val title: String,
+    val imageUrl: String,
+    val category: RecipeCategory,
+    val cookingSteps: String
 )
 
 @Composable
@@ -20,3 +27,13 @@ fun RecipeCategory.label(): String = when (this) {
     RecipeCategory.INDIAN -> stringResource(R.string.category_title_indian)
 }
 
+fun List<Recipe>.toUiItems(): ImmutableList<RecipeUiItem> =
+    this.map { it.toRecipeUiItem() }.toImmutableList()
+
+fun Recipe.toRecipeUiItem(): RecipeUiItem = RecipeUiItem(
+    this.id,
+    this.title,
+    this.imageUrl,
+    RecipeCategory.valueOf(this.category.uppercase()),
+    this.cookingSteps
+)
